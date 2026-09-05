dcdc7c5c99908529b1892c155b7948f0428e1133
Thabiso Kolojane <kt24019741@biust.ac.bw>
2026-09-05 20:41:54 +0200

    Update firebaseConfig.js

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -2,10 +2,11 @@
 // between the { } below, replacing the placeholder values. Nothing else in this
 // project needs to change.
 export const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_PROJECT_ID.appspot.com",
-  messagingSenderId: "YOUR_SENDER_ID",
-  appId: "YOUR_APP_ID",
+  apiKey: "AIzaSyDKeajvulkmOGlYsyI0_Vy7L_8h0mF3l2o",
+  authDomain: "log-issues.firebaseapp.com",
+  projectId: "log-issues",
+  storageBucket: "log-issues.firebasestorage.app",
+  messagingSenderId: "504537174305",
+  appId: "1:504537174305:web:8c6ebc5b36b52df56a6e60",
+  measurementId: "G-8FM6JHZN5S"
 };
